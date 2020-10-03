1dbf0da0fcb8d200b41843f77c400ac4754397d9
AmitPress <amitkrsurjo@gmail.com>
2020-10-03 14:17:59 +0600

    readme and helloworld

---
+++ helloworld.php
@@ -0,0 +1,8 @@
+<?php
+//use print for printing anything in the console
+print("hello world");
+//echo does the same thing, but conventionally most of the developer follows echo
+echo "\n"; // escape sequence also works here
+echo "hi";
+
+?>
---
+++ readme.md
@@ -0,0 +1,5 @@
+**This repo is for learing php,**
+
+I will be uploading file with comments and comments will be supper useful.
+
+And one thing again many codes will also be commented out. Please test them after uncommenting.
