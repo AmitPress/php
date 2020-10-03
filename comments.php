83b9c4f3ecaee16f1cdfd9b94be38c587092972b
AmitPress <amitkrsurjo@gmail.com>
2020-10-03 14:47:29 +0600

    commenting style

---
+++ comments.php
@@ -0,0 +1,20 @@
+<?php
+// use forward slash used as single line comment
+
+/**
+ * use forward slash double stars to make documentation comment like (/** )
+ */
+
+/*
+    use onle forward slash and a star to make generic multiple line comment
+    and use star forward slash to close it
+*/
+
+
+// while writing documentation 
+// always try to write documentation comment
+// and again try to do commenting like this file
+
+// again, use documention comment before function not instead in the function
+
+// use single comment in the function to reduce complexity of reading the code
