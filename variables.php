924ab8bde7c7b340b32f11d05b04072db787b4d0
AmitPress <amitkrsurjo@gmail.com>
2020-10-04 23:04:27 +0600

    variables.php updated

--- variables.php
+++ variables.php
@@ -25,3 +25,34 @@ var_dump($notnullval);//null
 /*
 so to delete variable just assign variable null and never used uninitialized variable
 */
+
+//variables can be assigned to another variable
+echo "\n\n\n\n\n";
+//copying variables
+$pink = 'pink';
+$yellow = $pink;
+
+echo $yellow;
+
+//reference to variables
+echo "\n \n \n";
+
+$apple = 'red'; //red
+
+$guava = 'green';//green
+
+$apple = &$guava;//green
+
+$apple = 'black';//black
+
+echo $guava;//black
+
+//duck typing
+
+echo "\n\n\n\n\n";
+$pinned = true;
+var_dump($pinned);
+$pinned = "is not true";
+var_dump($pinned);
+
+//so variables are loosely typed
